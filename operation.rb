f439f6df148e6a05a6e834d53b9c35a9deb9ff5f
biancamutaquiha <biafersil@gmail.com>
2022-05-09 12:04:35 +0200

    add initialize to operation

--- operation.rb
+++ operation.rb
@@ -1,4 +1,9 @@
-class Operation    
+class Operation
+  def initialize(left_operation, right_operation)
+    @left_operation = left_operation
+    @right_operation = right_operation
+  end
+
   def result; end
   def to_s; end
 end
