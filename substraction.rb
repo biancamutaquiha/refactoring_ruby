6566c8d51faf344a74aa0be81f35423dedeb3f13
biancamutaquiha <biafersil@gmail.com>
2022-05-09 12:05:51 +0200

    add substraction operation

---
+++ substraction.rb
@@ -0,0 +1,9 @@
+class Substraction < Operation
+  def result
+    @left_operation.result - @right_operation.result
+  end
+
+  def to_s
+    "(#{@left_operation.to_s} - #{@right_operation.to_s})"
+  end
+end
